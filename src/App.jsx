4b48221fa906e16b7c19e7fa9323fa23eb380e72
PITT1 <petroleo.plta.97@gmail.com>
2024-04-05 15:19:55 -0400

    app lista solo falta cargar el backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { useState } from 'react';
 import './App.css'
 
-
-
 function App() {
-  let textIn = ""
+  const [textOut, setTextOut] = useState("");
+  const [textIn, setTextIn] = useState("");
 
   const URL = "http://127.0.0.1:8000/traducir";
 
@@ -14,21 +14,22 @@ function App() {
 
   const options = {
     method: 'POST',
-    header: {
+    headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
   }
 
-  
-
   const hanleChange = (event) => {
-    textIn = event.target.value;
-    console.log(textIn)
+    setTextIn(event.target.value);
   };
 
   const translate = () => {
     fetch(URL, options)
+    .then(res => res.json())
+    .then(res => {
+      setTextOut(res.traduccion);
+    })
   }
 
   return (
@@ -36,7 +37,7 @@ function App() {
       <div className='container'>
         <textarea onChange={hanleChange} name="input-translate" cols="50" rows="10" autoCapitalize='sentences' required placeholder='Escribe algo para traducir'></textarea>
         <button onClick={translate}>Traducir</button>
-        <div></div>
+        <div>{textOut}</div>
       </div>
     </main>
   )
